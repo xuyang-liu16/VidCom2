3cc105503005195ebf3e82222837992056c70dd3
guixiyan <160238081+sakura-gui@users.noreply.github.com>
2025-12-07 16:31:38 +0800

    Add files via upload

---
+++ visual_head/LLaVA-NeXT/llava/model/__init__.py
@@ -0,0 +1,14 @@
+import os
+
+AVAILABLE_MODELS = {
+    "llava_llama": "LlavaLlamaForCausalLM, LlavaConfig",
+    "llava_qwen": "LlavaQwenForCausalLM, LlavaQwenConfig",
+    "llava_mistral": "LlavaMistralForCausalLM, LlavaMistralConfig",
+    "llava_mixtral": "LlavaMixtralForCausalLM, LlavaMixtralConfig",
+    # "llava_qwen_moe": "LlavaQwenMoeForCausalLM, LlavaQwenMoeConfig",    
+    # Add other models as needed
+}
+
+for model_name, model_classes in AVAILABLE_MODELS.items():
+        exec(f"from .language_model.{model_name} import {model_classes}")
+    
