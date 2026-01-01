c06193fcacd67ccec6b598d72fc7ef7080ad7c47
xuyang-liu16 <liuxuyang@stu.scu.edu.cn>
2026-01-01 04:16:44 -0800

    Add token_compressor wrappers for compatibility

---
+++ token_compressor/models/llava.py
@@ -0,0 +1,2 @@
+"""Compatibility wrapper for Llava VidCom2 hooks."""
+from token_compressor.vidcom2.models.llava import *
---
+++ token_compressor/models/qwen2_vl.py
@@ -0,0 +1,2 @@
+"""Compatibility wrapper for Qwen2-VL VidCom2 hooks."""
+from token_compressor.vidcom2.models.qwen2_vl import *
