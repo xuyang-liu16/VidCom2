d0ec5e0be526cad941eed421472eb3cfe4446cd2
xuyang-liu16 <liuxuyang@stu.scu.edu.cn>
2026-01-01 04:24:59 -0800

    Remove redundant token_compressor models wrappers

--- token_compressor/models/llava.py
+++
@@ -1,2 +0,0 @@
-"""Compatibility wrapper for Llava VidCom2 hooks."""
-from token_compressor.vidcom2.models.llava import *
--- token_compressor/models/qwen2_vl.py
+++
@@ -1,2 +0,0 @@
-"""Compatibility wrapper for Qwen2-VL VidCom2 hooks."""
-from token_compressor.vidcom2.models.qwen2_vl import *
